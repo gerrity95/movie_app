67da85983309b24aaf47091e265ef9edddb8c21b
Mark Gerrity <mark_g@LAPTOP-7JV77MGM.localdomain>
2022-10-19 20:49:28 +0100

    Refactor of routes and some UI improvements

--- src/routes/reset.routes.js
+++ src/routes/reset.routes.js
@@ -3,14 +3,12 @@ const router = express.Router();
 const presetController = require('../controllers/password_reset.controller');
 
 // GET method routes
-router.get('/password_reset', presetController.password_reset);
-// router.get('/:userId/:token', presetController.password_update);
+router.get('/', presetController.password_reset);
+router.get('/:userId/:token', presetController.password_update);
 
 // POST method routes
-router.post('/password_reset', presetController.password_reset_post);
+router.post('/', presetController.password_reset_post);
 router.post('/:userId/:token', presetController.password_update_post);
 
 
-module.exports = {
-  router: router,
-};
+module.exports = router;
--- src/routes/tmdb_api.routes.js
+++ src/routes/tmdb_api.routes.js
@@ -5,13 +5,11 @@ const tmdbapiController = require('../controllers/tmdbapi.controller');
 const genericHelpers = require('../utils/generic_helpers');
 
 // GET method routes
-router.get('/user/recommended_shows', genericHelpers.isLoggedIn,
+router.get('/recommended_shows', genericHelpers.isLoggedIn,
     tmdbapiController.recommended_shows);
 
 // POST method routes
-router.post('/user/submit_rating', genericHelpers.isLoggedIn, tmdbapiController.submit_rating);
+router.post('/submit_rating', genericHelpers.isLoggedIn, tmdbapiController.submit_rating);
 
 
-module.exports = {
-  router: router,
-};
+module.exports = router;
